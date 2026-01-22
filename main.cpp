2908609a5bb0ec30f2ce6775f9f12fed5a704e6e
Rudra152005 <www.rudra152005@gmail.com>
2026-01-22 16:30:40 +0530

    cpp code push

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter operator (+, -, *, /, &, |, <, >): ";
+    cin >> op;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    switch (op) {
+        case '+':
+            cout << "Result = " << a + b;
+            break;
+
+        case '-':
+            cout << "Result = " << a - b;
+            break;
+
+        case '*':
+            cout << "Result = " << a * b;
+            break;
+
+        case '/':
+            if (b != 0)
+                cout << "Result = " << a / b;
+            else
+                cout << "Division by zero not allowed";
+            break;
+
+        case '&':
+            cout << "Bitwise AND = " << (a & b);
+            break;
+
+        case '|':
+            cout << "Bitwise OR = " << (a | b);
+            break;
+
+        case '<':   
+            cout << "Left Shift = " << (a << b);
+            break;
+
+        case '>':   
+            cout << "Right Shift = " << (a >> b);
+            break;
+
+        default:
+            cout << "Invalid operator!";
+    }
+
+    return 0;
+}
+
